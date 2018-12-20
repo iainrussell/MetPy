4f3a6f8d3d38fd8bb4864d6953326071fd6006e1
Travis CI <travis@nobody.org>
2018-12-20 01:03:47 +0000

    Deploy dev to GitHub Pages

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.1+9.gd0d44e8',
+    VERSION: '0.9.1+78.g3fcd18e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
